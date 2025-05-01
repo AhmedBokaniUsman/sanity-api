8517dbca8be7fdfccf5e19930aba670738a5d74a
AhmedBokaniUsman <ahmedbokaniusman5@gmail.com>
2025-05-01 12:20:52 +0100

    Initial commit for Sanity API

---
+++ schemaTypes/property.js
@@ -0,0 +1,44 @@
+// schemas/property.js (or the file where your property schema is defined)
+
+export default {
+    name: 'property',
+    title: 'Property',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Title',
+        type: 'string',
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'text',
+      },
+      {
+        name: 'price',
+        title: 'Price',
+        type: 'number',
+      },
+      {
+        name: 'location',
+        title: 'Location',
+        type: 'string',
+      },
+      {
+        name: 'images',
+        title: 'Images',
+        type: 'array',
+        of: [
+          {
+            type: 'image',
+            title: 'Image',
+            options: {
+              hotspot: true, // Allows you to crop and adjust images in Sanity Studio
+            },
+          },
+        ],
+      },
+    ],
+  };
+  
